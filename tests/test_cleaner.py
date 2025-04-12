625e36ef465c42b20518e46f13c005a998bf439e
Khaled15 <khaled.ameen11@gmail.com>
2025-04-12 23:40:18 +0200

    adding new def string and spaces

--- tests/test_cleaner.py
+++ tests/test_cleaner.py
@@ -12,3 +12,11 @@ def test_cleaner_with_empty_string():
 
 def test_cleaner_with_string_with_spaces():
     assert cleaner(" ") == ""
+
+
+def test_cleaner_with_string_with_spaces_and_numbers():
+    assert cleaner("123") == ""
+
+
+def test_cleaner_with_string_with_spaces_and_numbers_and_letters():
+    assert cleaner("123abc") == ""
